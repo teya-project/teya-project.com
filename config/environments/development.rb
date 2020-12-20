14d93bf6042ddab7d73456c0dab35e3d0f651a11
teya-project.com <teyaproject.rb@gmail.com>
2020-12-20 04:13:42 +0300

    add config.hosts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,4 +75,5 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
   #
   config.hosts << "beta.teya-project.com"
+  config.hosts << "0.0.0.0"
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,4 +118,5 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.hosts << "teya-project.com"
+  config.hosts << "0.0.0.0"
 end
